aac4df6d9259455f7486f8f2a16cc1c2b6516737
Maxim Zhuk <zhukme84@gmail.com>
2024-02-06 16:22:51 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+string GetStringFromArrayChar(char[,] array)
+{
+    string str = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            str += array[i, j];    
+        }
+    }
+    return str;
+}
+
+char[,] chars = {
+    {'H', 'e', 'l', 'l', 'o'},
+    {'W', 'o', 'r', 'l', 'd'}
+};
+Console.WriteLine($"Итоговая строка {GetStringFromArrayChar(chars)} ");
